77f6c76094609acda05c80fa4a068844201f777b
mosabbir.duet <mosabbir.duet@gmail.com>
2023-03-04 00:10:18 +0600

    display ai tool in card

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 const loadAItools = () => {
-    fetch('https://openapi.programming-hero.com/api/ai/tools').then(res => res.json())
-    .then(data => console.log(data));
-}
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
+    .then((data) => displayAiTools(data.data.tools));
+};
+
+const displayAiTools = (tools) => {
+  console.log(tools);
+  const aiToolsContainer = document.getElementById("ai-universe-tool");
+  aiToolsContainer.innerHTML = "";
+  tools.forEach((tool) => {
+    aiToolsContainer.innerHTML += `
+            <div class="col">
+                <div class="card h-100 p-3">
+                    <img src="${tool.image}" class="card-img-top rounded-3" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Features</h5>
+                        <ol>${tool.features.map((feature) => {
+                        return `<li>${feature}</li>`;
+                        }).join("")}
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+};
+
+
 
 loadAItools();
